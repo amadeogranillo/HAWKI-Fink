478d5f6edaa2283800e31440bdd5cb621edb3710
Programmhacker <chrisi.scheidler@gmx.de>
2025-06-22 19:07:25 +0200

    GWDG Provider: Add support for custom model parameters

--- resources/views/partials/login/authForms.blade.php
+++ resources/views/partials/login/authForms.blade.php
@@ -1,7 +1,11 @@
+@php
+    $loginButtonClass = 'modern-login-button';
+@endphp
+
 @if($authenticationMethod === 'OIDC')
     <form class="form-column" method="post" id="loginForm-OIDC" action="/req/login-oidc">
         @csrf
-        <button id="loginButton" class="btn-lg align-end top-gap-1">{{ $translation['Login'] }}</button>
+        <button id="loginButton" class="{{ $loginButtonClass }}">{{ $translation['Login'] }}</button>
     </form>
 @elseif($authenticationMethod === 'LDAP' || $authenticationMethod === 'TestAuth')
     <form class="form-column" id="loginForm-LDAP">
@@ -13,12 +17,12 @@
     </form>
     <div id="login-Button-panel">
         <div id="login-message"></div>
-        <button id="loginButton" class="btn-lg-fill align-end top-gap-1" type="button" onclick="LoginLDAP()">{{ $translation['Login'] }}</button>
+        <button id="loginButton" class="{{ $loginButtonClass }}" type="button" onclick="LoginLDAP()">{{ $translation['Login'] }}</button>
     </div>
 @elseif($authenticationMethod === 'Shibboleth')
     <form class="form-column" method="post" id="loginForm-Shib" action="/req/login-shibboleth">
         @csrf
-        <button id="loginButton" class="btn-lg-fill align-end top-gap-1" type="submit" name="submit">{{ $translation['Login'] }}</button >
+        <button id="loginButton" class="{{ $loginButtonClass }}" type="submit" name="submit">{{ $translation['Login'] }}</button >
     </form>
 @else
     No authentication method defined
